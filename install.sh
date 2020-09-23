32128976181394506139b3289677b8627abd8d2b
lalyos <lalyos@yahoo.com>
2020-09-23 14:15:58 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get install -y nginx
-echo "<h2>mingyart ebed</h2>" > /var/www/html/index.nginx-debian.html
+echo "<h2>mingyart vacsora</h2>" > /var/www/html/index.nginx-debian.html
 
-echo "Host: $HOST" >> /var/www/html/index.nginx-debian.html
+echo "Host: $HOSTNAME" >> /var/www/html/index.nginx-debian.html
