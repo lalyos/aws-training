98bd375f071a5ca96c4e4d414b5a522b1f1f76c0
lalyos <lalyos@yahoo.com>
2020-09-24 13:42:19 +0200

    use securioty group name instead of id

--- run-instance.sh
+++ run-instance.sh
@@ -4,7 +4,7 @@ aws ec2 run-instances \
   --image-id "ami-0c960b947cbb2dd16" \
   --count 1 \
   --key-name "id_rsa" \
-  --security-group-ids '["sg-073b7d4956213ed04"]' \
+  --security-groups launch-wizard-1 \
   --instance-type "t2.micro" \
   --tag-specifications '[{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"ebedutan"},{"Key":"owner","Value":"lalyos"}]},{"ResourceType":"volume","Tags":[{"Key":"Name","Value":"ebedutan"},{"Key":"owner","Value":"lalyos"}]}]' \
   --user-data file://user-data.sh
